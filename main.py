8581b5d6af0c1050f4273add0611b616a88ce858
GuFFy12 <jokers_hello_0d@icloud.com>
2026-02-25 19:22:12 +0000

    ruff gives errors; hooks fix

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def main():
-    print("Hello from python-template!")
+# If you encounter unwanted Ruff errors, simply add the codes to the ignore section in .ruff.toml  # noqa: D100
+
+
+def main():  # noqa: ANN201, D103
+    print("Hello from python-template!")  # noqa: T201
 
 
 if __name__ == "__main__":
